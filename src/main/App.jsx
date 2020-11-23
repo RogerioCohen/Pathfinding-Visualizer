a12790f0807f6e1a81aca416f573066c854468c4
Maria Luiza C. Wuillaume <marialuizacw@gmail.com>
2020-11-22 21:26:51 -0300

    Start

---
+++ src/main/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header'
+import Subtitle from '../components/Subtitle'
+import Grid from '../main/Grid'
+import Paint from '../components/Paint'
+import Steps from '../components/Steps'
+
+
+
+const App =  function() {
+    return (
+        <React.Fragment>
+        </React.Fragment>
+
+    )
+}
+
+
+export default App
+
+
