b7c28ca9aaefbdf249779ad4ddacf276249edd87
RogerioCohen <yfromeiro@gmail.com>
2020-12-07 15:37:44 -0300

    Header changes
    Coloquei a funcionalidade de poder clicar em vários botões sem atualizar a página e coloquei para retornar os algoritmos(do geito que deveria mesmo).

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,77 +1,93 @@
 import React, {Component} from 'react';
 import './Header.css'
 import algorithms from '../algorithms/surroundingWalls'
-
+import Astar from '../algorithms/Astar' 
+import bfs from '../algorithms/bfs'
+import dfs from '../algorithms/dfs'
+import dijkstra from '../algorithms/dijkstra'
 
 let selectedAlgorithm = null
+
+
 class Header extends React.Component{
     
     state = {
         buttonChecker: true,
-        possibleColors : ['green',"#FF7070"],
-        Dij:0,
-        Astar:0,
-        BFS:0,
-        DFS:0
+        nowButton : null
     }
+
     handleClickDij(event){
-        console.log('A');
-        event.target.style.background = "#FF7070";
-        this.setState({buttonChecker: false})
-        console.log(algorithms)
-        selectedAlgorithm = algorithms
+        console.log('Dij');
+        
+        selectedAlgorithm = dijkstra
+        
     }
     
     handleClickAs(event){
-        console.log('B');
-        event.target.style.background = "#FF7070";
-        this.setState({buttonChecker: false})
+        console.log('Astar');
+        selectedAlgorithm = Astar
     }
 
     handleClickBFS(event){
-        console.log('C');
-        event.target.style.background = "#FF7070";
-        this.setState({buttonChecker: false})
+        console.log('BFS');
+        selectedAlgorithm = bfs
     }
 
     handleClickDFS(event){
-        console.log('D');
-        event.target.style.background = "#FF7070";
-        this.setState({buttonChecker: false})
+        console.log('DFS');
+        selectedAlgorithm = dfs
     } 
-     
+    
+
+
+
+    teste(event){
+        if(this.state.nowButton === null){
+           this.setState({nowButton: event.target})  
+           event.target.style.background = '#FF7070'
+        }else{
+            this.state.nowButton.style.background = 'rgb(117, 240, 144)'
+            event.target.style.background = '#FF7070'
+            this.setState({nowButton: event.target})
+        }
+        
+    }    
+        
+       
     render(){
         return(
             <div className = 'header'>
                     
                 <button className='headerBtn' id = 'Dij' onClick = {event => {
-                    if( this.state.buttonChecker ){
-                        return this.handleClickDij(event);
-                    }  }
-                }>
+                    this.handleClickDij(event);
+                    this.teste(event)
+                    }
+                    }>
                     View Dijstra                        
                 </button> 
+                     
+                
             
                 <button className='headerBtn' id = 'Astar' onClick = {event => {
-                    if( this.state.buttonChecker ){
-                        return this.handleClickAs(event);
-                    }  }
-                }>
+                    this.handleClickAs(event);
+                    this.teste(event)
+                    }
+                    }>
                     View A*
                 </button>                                          
                                       
              
                 <button className='headerBtn'   id =  'BFS' onClick = {event => {
-                    if( this.state.buttonChecker ){
-                        return this.handleClickBFS(event);
-                    }  }}>
+                    this.handleClickBFS(event);
+                    this.teste(event)
+                    }}>
                         View BFS
                 </button>    
 
                 <button  className='headerBtn' id =  'DFS' onClick = {event =>{
-                    if( this.state.buttonChecker ){
-                        return this.handleClickDFS(event);
-                    }  } }>
+                    this.handleClickDFS(event);
+                    this.teste(event)
+                    } }>
                     View DFS
                 </button>      
                 <h1> Pathfinding Visualizer</h1>
